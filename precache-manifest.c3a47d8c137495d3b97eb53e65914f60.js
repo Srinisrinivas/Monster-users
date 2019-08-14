8935bf7e4af278689d5fc3999051c9665182ff40
Srinisrinivas <srini.srinivas61@gmail.com>
2019-08-15 02:45:48 +0530

    Updates

---
+++ precache-manifest.c3a47d8c137495d3b97eb53e65914f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6dd8e1d87c2a43aefb0755de7cb083",
+    "url": "/Monster-users/index.html"
+  },
+  {
+    "revision": "fe408fcf22dfec111f24",
+    "url": "/Monster-users/static/css/main.7b7b8145.chunk.css"
+  },
+  {
+    "revision": "c9f0ff8d4457f60fa73e",
+    "url": "/Monster-users/static/js/2.55a63239.chunk.js"
+  },
+  {
+    "revision": "fe408fcf22dfec111f24",
+    "url": "/Monster-users/static/js/main.7d55d99a.chunk.js"
+  },
+  {
+    "revision": "c796d73007e815931ae3",
+    "url": "/Monster-users/static/js/runtime~main.6b984d23.js"
+  }
+]);
